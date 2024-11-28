b4e7e170505c61dec5e128e5505caebeede78132
Frederico Rodrigues <frederico.rodrigues@ebankit.com>
2024-11-28 00:34:16 +0000

    updade

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -3,7 +3,5 @@ describe('Basic specs', () => {
     cy.visit('/')
   })
 
-  it('Check for jokes link', () => {
-    cy.get('.jokes-link').should('have.attr', 'href').and('include', 'api/joke')
-  })
+
 })
