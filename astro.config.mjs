ec7237d9c1763898ab0ea716a8f887f48b05f3b9
Frederico Rodrigues <frederico.rodrigues@gmail.com>
2024-11-28 00:10:08 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
  output: 'server',
   server: {
-		headers: {
-			"Access-Control-Allow-Origin": "*",
+     headers: {
+      "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": "true",
       "Access-Control-Allow-Origin":"*",
       "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT" ,
       "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-		}
-  },
-  
+     }
+    },
+    adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
 });
